34fc2792fa6e32a6a841033ab3a8a166dc86be20
Eugenia Andreyeva <zey_a@MacBook-Pro-Eugenia.local>
2021-12-01 17:46:22 +0300

    first addition main class frame

---
+++ laba 2/src/com/company2/MainFrame.java
@@ -0,0 +1,88 @@
+package com.company2;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Главный класс приложения, он же класс фрейма
+public class MainFrame extends JFrame {
+    // Размеры окна приложения в виде констант
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 320;
+    //Результаты вычислений
+    private Double result;
+    //Сумма результатов
+    private Double Sum = 0.;
+    private int formulaId = 1;
+
+    // Формула №1 для рассчѐта
+    public Double calculate1(Double x, Double y, Double z) {
+        return Math.pow(Math.cos(Math.pow(Math.E,x))+Math.log(Math.pow(1+y,2))+
+                Math.sqrt(Math.pow(Math.E,Math.cos(x))+Math.pow(Math.sin(Math.PI*z),2))+
+                Math.sqrt(1/x)+Math.cos(Math.pow(y,2)),Math.sin(z));
+    }
+
+    // Формула №2 для рассчѐта
+    public Double calculate2(Double x, Double y, Double z) {
+        return Math.pow(1+Math.pow(x,2),1/y)/Math.pow(Math.E,Math.sin(z)+x);
+    }
+
+    // Конструктор класса
+    public MainFrame() {
+        super("Вычисление формулы");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+// Отцентрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH) / 2,
+                (kit.getScreenSize().height - HEIGHT) / 2);
+        hboxFormulaType.add(Box.createHorizontalGlue());
+        addRadioButton("Формула 1", 1);
+        addRadioButton("Формула 2", 2);
+
+// Создать область с полями ввода для X и Y
+        JLabel labelForX = new JLabel("X:");
+        textFieldX = new JTextField("0", 5);
+        textFieldX.setMaximumSize(textFieldX.getPreferredSize());
+        JLabel labelForY = new JLabel("Y:");
+        textFieldY = new JTextField("0", 5);
+        textFieldY.setMaximumSize(textFieldY.getPreferredSize());
+        JLabel labelForZ = new JLabel("Z:");
+        textFieldZ = new JTextField("0", 5);
+        textFieldZ.setMaximumSize((textFieldZ.getPreferredSize()));
+        Box hboxVariables = Box.createHorizontalBox();
+        hboxVariables.setBorder(BorderFactory.createLineBorder(Color.RED));
+        hboxVariables.add(Box.createHorizontalGlue());
+        hboxVariables.add(labelForX);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldX);
+        hboxVariables.add(Box.createHorizontalStrut(50));
+        hboxVariables.add(labelForY);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldY);
+        hboxVariables.add(Box.createHorizontalStrut(50));
+        hboxVariables.add(labelForZ);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldZ);
+        hboxVariables.add(Box.createHorizontalGlue());
+// Создать область для вывода результата
+        JLabel labelForResult = new JLabel("Результат:");
+        textFieldResult = new JTextField("0", 15);
+        textFieldResult.setMaximumSize(textFieldResult.getPreferredSize());
+        JLabel labelForM = new JLabel("Sum:");
+        textFieldM = new JTextField("0", 15);
+        textFieldM.setMaximumSize(textFieldM.getPreferredSize());
+        Box hboxResult = Box.createHorizontalBox();
+        hboxResult.add(Box.createHorizontalGlue());
+        hboxResult.add(labelForResult);
+        hboxResult.add(Box.createHorizontalStrut(10));
+        hboxResult.add(textFieldResult);
+        hboxResult.add(Box.createHorizontalStrut(20));
+        hboxResult.add(labelForM);
+        hboxResult.add(Box.createHorizontalStrut(10));
+        hboxResult.add(textFieldM);
+        hboxResult.add(Box.createHorizontalGlue());
+        hboxResult.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+
+}
