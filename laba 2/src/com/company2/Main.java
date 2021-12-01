bffd50314616be299203a14ed9a1b5bbc3dbe7f0
Eugenia Andreyeva <zey_a@MacBook-Pro-Eugenia.local>
2021-12-01 17:42:23 +0300

    added the main method of the class

---
+++ laba 2/src/com/company2/Main.java
@@ -0,0 +1,13 @@
+package com.company2;
+
+import javax.swing.JFrame;
+
+public class Main {
+    // Главный метод класса
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
